87459e88661a52b31bac967e93cd3f9d7ce9cb45
Dmitry Ishkov <dmitryishkov27@gmail.com>
2023-05-26 00:47:16 +0300

    solved #1, #2 task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
+import user from 'path/to/user.json'
+import data from 'path/to/data.json'
+import { Profile } from './Profile/Profile'
+import { Statistics } from './Statistics/Statistics';
+
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
+        gap: '20px',
         fontSize: 40,
         color: '#010101'
       }}
     >
-      React homework template
+      <Profile
+        avatar={user.avatar}
+        username={user.username}
+        location={user.location}
+        stats={user.stats}
+        tag={user.tag}
+      />
+      <Statistics stats={data} />
     </div>
   );
 };
